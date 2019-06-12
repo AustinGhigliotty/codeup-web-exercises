cfd9ffe8bc6d989ea6d81ca16cc0052f8ef73fad
Austin Ghigliotty <yolarr@Austins-MacBook-Pro.local>
2019-06-12 16:52:03 -0500

    first commit for external js

---
+++ js/external.js
@@ -0,0 +1,15 @@
+console.log("Hello from external JavaScript");
+alert('Welcome to my Website!');
+var color = prompt("What is your favorite color?");
+alert("Great, " + color + " is my favorite color too!");
+
+var movieMermaid = prompt("How many days for The Little Mermaid?");
+var movieBear = prompt("How many days for Brother Bear?");
+var movieHercules = prompt("How many days for Hercules?");
+alert("At $3 a day, you would pay $" + ((3 * movieMermaid) + (3 * movieBear) + (3 * movieHercules)));
+
+var hoursFacebook = prompt("How many hours working for Facebook?");
+var hoursGoogle = prompt("How many hours working for Google?");
+var hoursAmazon = prompt("How many hours working for Amazon?");
+alert("You will get paid $" + ((400 * hoursGoogle) + (350 * hoursFacebook) + (380 * hoursAmazon)));
+
